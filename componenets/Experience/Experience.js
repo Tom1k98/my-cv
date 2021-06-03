03a66f718fcbe674ef20427f7d8f4ef57da4c978
Tomas Storc <tomas.storc@gmail.com>
2021-06-03 21:15:26 +0200

    basic layout for experience and text

--- componenets/Experience/Experience.js
+++ componenets/Experience/Experience.js
@@ -1,11 +1,24 @@
 import React from "react";
 import expStyles from "./Experience.module.scss";
-import { header } from "./experienceText";
+import { header, experiences } from "./experienceText";
 
 const Experience = ({ language }) => {
   return (
     <div name="experience" className={expStyles.experience}>
       <h2>{language === "cz" ? header.cz : header.en}</h2>
+      <div className={expStyles.experienceflex}>
+        {language === "cz"
+          ? experiences.cz.map((exp) => (
+              <div>
+                <h3>{exp.title}</h3>
+              </div>
+            ))
+          : experience.en.map((exp) => (
+              <div>
+                <h3>{exp.title}</h3>
+              </div>
+            ))}
+      </div>
     </div>
   );
 };
--- componenets/Experience/experienceText.js
+++ componenets/Experience/experienceText.js
@@ -2,3 +2,64 @@ export const header = {
   en: "Work Experience",
   cz: "Pracovní zkušenosti",
 };
+
+export const experiences = {
+  en: [
+    {
+      title: "Aufeer design s.r.o.",
+      date: "09/2017-now",
+      role: "IT specialist",
+      description:
+        "Linux server administration Support for Windows/Linux users computers",
+    },
+    {
+      title: "Seznam.cz a.s.",
+      date: "04/2019 – 02/2020",
+      role: "System Engineer",
+      description:
+        "Linux server administration Releasing and debugging new version of apps Automatization with Ansible Python scripting",
+    },
+    {
+      title: "Atos IT Solutions",
+      date: "10/2020 – 05/2021",
+      role: "ICT consultant",
+      dexcription:
+        "Administration and installation of SIEM systems (IBM QRadar, Splunk) Implementation of MS Active Directory Instalation of IBM ISIM",
+    },
+    {
+      title: "Rockwell automation",
+      date: "06/2021 - now",
+      role: "Application Engineer",
+      description: "Node.JS programming TypeScript programming",
+    },
+  ],
+  cz: [
+    {
+      title: "Aufeer design s.r.o.",
+      date: "09/2017-současnost",
+      role: "IT specialista",
+      description:
+        "Správa serverů s OS Linux, support koncovým uživatelům s Linux a Windows stanicemi",
+    },
+    {
+      title: "Seznam.cz a.s.",
+      date: "04/2019 – 02/2020",
+      role: "System Engineer",
+      description:
+        "Správa serverů s OS Linux, nasazování a debugování aplikací, automatizace pomocí Ansiblu a Pythonu",
+    },
+    {
+      title: "Atos IT Solutions",
+      date: "10/2020 – 05/2021",
+      role: "ICT consultant",
+      dexcription:
+        "Adminstrace a instalace SIEM softwaru (IBM QRadar, Splunk) IInstalace a implementace MS Active Directory",
+    },
+    {
+      title: "Rockwell automation",
+      date: "06/2021 - současnost",
+      role: "Application Engineer",
+      description: "Programovaní v NodeJS a typescriptu",
+    },
+  ],
+};
