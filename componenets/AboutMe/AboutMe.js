e11ac303bab6e49cdfa13a49b4d080b926b5a360
Tomas Storc <tomas.storc@gmail.com>
2021-06-04 09:42:46 +0200

    useState to determine language

--- componenets/AboutMe/AboutMe.js
+++ componenets/AboutMe/AboutMe.js
@@ -1,19 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import aboutMeStyles from "./AboutMe.module.scss";
 import { heading, text } from "./AboutMeText";
 
 function AboutMe({ language }) {
+  const [getHeading, setHeading] = useState();
+  const [getText, setText] = useState();
+
   const { ref, inView } = useInView({
     threshold: 0,
   });
 
+  useState(() => {
+    setHeading(language === "cz" ? heading.cz : heading.en);
+    setText(language === "cz" ? text.cz : text.en);
+  }, [language]);
+
   return (
     <div ref={ref} className={inView && aboutMeStyles.aboutme} name="aboutme">
-      <h2>{language === "cz" ? heading.cz : heading.en}</h2>
-      <div className={aboutMeStyles.abouttext}>
-        {language === "cz" ? text.cz : text.en}
-      </div>
+      <h2>{getHeading}</h2>
+      <div className={aboutMeStyles.abouttext}>{getText}</div>
     </div>
   );
 }
