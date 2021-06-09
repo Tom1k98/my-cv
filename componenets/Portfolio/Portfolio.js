b752d6df16bc7c3813581bbfb7c44599e3a64d8b
Tomas Storc <tomas.storc@gmail.com>
2021-06-09 19:23:32 +0200

    Portfolio basic structure

---
+++ componenets/Portfolio/Portfolio.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import portfolioStyles from "./Portfolio.module.scss";
+
+const Portfolio = ({ language }) => {
+  return <div></div>;
+};
+
+export default Portfolio;
