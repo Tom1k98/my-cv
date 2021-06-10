8311b89ffd036b2c162ad14d5d0ac4f37d7a8ccb
Tomas Storc <tomas.storc@gmail.com>
2021-06-10 20:33:09 +0200

    text update

--- componenets/AboutMe/AboutMeText.js
+++ componenets/AboutMe/AboutMeText.js
@@ -4,6 +4,6 @@ export const heading = {
 };
 
 export const text = {
-  en: "My name is Tomáš and I have been working as a linux specialist for 4 years. Right now I am changing my professional career path to a software development. I started my journey as application engineer at Rockwell automation where I use Node.js and TypeScript. I also do my own projects if you are interested feel free to check out my GitHub.",
-  cz: "Jmenuji se Tomáš and pracoval jsem 4 roky jako Linux specialista. ",
+  en: "My name is Tomáš and I have been working as a linux specialist for 4 years. Right now I am changing my professional career path to a software development. I started my journey as application engineer at Rockwell Automation where I use Node.js and TypeScript. I also do my own projects if you are interested feel free to check out my GitHub.",
+  cz: "Jmenuji se Tomáš a pracoval jsem 4 roky jako Linux specialista. Nyní se snažím změnit své profesní zaměření. Chtěl bych se stát software developerem. Svojí cestu jsem začal jako vývojář ve společnosti Rockwell Automation, kde programuji v Node.js a typescriptu. Ve svém volném čase se věnuji svým osobním projektům, které naleznete na mém Githubu. ",
 };
