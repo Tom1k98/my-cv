0d97eba4d85f0ad1223c3ddc459f9df4619e5bfd
Tomas Storc <tomas.storc@gmail.com>
2021-06-22 13:21:03 +0200

    prtfolio img change

--- componenets/Portfolio/portfolioText.js
+++ componenets/Portfolio/portfolioText.js
@@ -1,7 +1,7 @@
 export const portfolio = {
   en: [
     {
-      img: "/img/mtnetcode.jpg",
+      img: "/img/mtnetcode.svg",
       imgpos: "top",
       heading: "MTnetcode website",
       text: "fully responsive web site created using HTML, CSS and vanilla JavaScript on frontend. There is also backend resposible for sending email message, written in Node.js",
@@ -18,7 +18,7 @@ export const portfolio = {
 
   cz: [
     {
-      img: "/img/mtnetcode.jpg",
+      img: "/img/mtnetcode.svg",
       imgpos: "top",
       heading: "Webová stránka MTnetcode",
       text: "Plně responzivní webová stránka vytvořená v HTML, CSS a čistém JavaScriptu na frontendu. Backend je napsám v Node.js a má na starost odeslání emailu",
