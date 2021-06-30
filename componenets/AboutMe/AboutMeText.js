1a2c2cac4aab91ae008a4a70ef3470e89167f899
Tomas Storc <tomas.storc@gmail.com>
2021-06-30 17:36:07 +0200

    typo

--- componenets/AboutMe/AboutMeText.js
+++ componenets/AboutMe/AboutMeText.js
@@ -4,6 +4,6 @@ export const heading = {
 };
 
 export const text = {
-  en: "My name is Tomáš and i have been working as a Linux adminstrator for four years. What i love the most is automation, mostly with Python. I am also interested in server monitoring, where i am using Zabbix software. Recently i have started new hobby, web development so far, i made few projects, you can see them at Portfolio section or on my GitHub.",
+  en: "My name is Tomáš and i have been working as a Linux adminstrator for four years. What i love the most is automation, mostly with Python. I am also interested in server monitoring, where i am using Zabbix software. Recently i have started new hobby, web development. So far, i made few projects, you can see them at Portfolio section or on my GitHub.",
   cz: "Jmenuji se Tomáš a pracoval jsem 4 roky jako Linux specialista. Co mám rán nejvíce je automatizace, kde používám především PYthon. Dále mě zajímá především monitorování serverů, kde používám software Zabbix. V nedávné době jsem začal nový koníček, vývoj webů, již jsem udělal několik stránek/projektů, které si můžete prohlédnout v sekci Portfolio, nebo na mém GitHubu ",
 };
