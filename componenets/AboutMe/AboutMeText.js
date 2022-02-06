d9b3fca03edb79e76fd669898001858ae90fb143
Tomas Storc <tomas.storc@gmail.com>
2022-02-06 18:25:58 +0100

    edited text

--- componenets/AboutMe/AboutMeText.js
+++ componenets/AboutMe/AboutMeText.js
@@ -4,6 +4,6 @@ export const heading = {
 };
 
 export const text = {
-  en: "My name is Tomáš and i have been working as a Linux adminstrator for four years. What i love the most is automation, mostly with Python. I am also interested in cloud administration, especially Azure. Recently i have started new hobby, web development. So far, i made few projects, you can see them at Portfolio section or on my GitHub.",
-  cz: "Jmenuji se Tomáš a pracoval jsem 4 roky jako Linux specialista. Co mám rán nejvíce je automatizace, kde používám především Python. Dále mě zajímá správa cloud prostředí, především Azure. V nedávné době jsem začal nový koníček, vývoj webů, již jsem udělal několik stránek/projektů, které si můžete prohlédnout v sekci Portfolio, nebo na mém GitHubu ",
+  en: "My name is Tomáš and i have been working as a Linux adminstrator for four years. I have also experience in automation, mostly with Python. I am also interested in cloud administration, especially Azure. But what i love the most is web development. I have decided to start University focused on software devolpment and i would like to find a job in this field. ",
+  cz: "Jmenuji se Tomáš a pracoval jsem 4 roky jako Linux specialista. Dále mám zkušenost s automatizací, kde používám především Python. Ve své současné práci spravuji Azure prostředí. Co mě ale baví nejvíce je vývoj webu. Rozhodl jsem se začít studovat vysokou školu zaměřenou právě na vývoj softwaru a chtěl bych se tomuto oboru začít věnovat i po kariérní stránce.",
 };
