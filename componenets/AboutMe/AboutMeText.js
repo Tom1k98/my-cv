58ecd9b1c243ce45b902ef8794797fe4c4c09c77
Tomas Storc <tomas.storc@gmail.com>
2022-05-28 21:11:26 +0200

    changed text

--- componenets/AboutMe/AboutMeText.js
+++ componenets/AboutMe/AboutMeText.js
@@ -4,6 +4,6 @@ export const heading = {
 };
 
 export const text = {
-  en: "My name is Tomáš and i have been working as a Linux adminstrator for four years. I have also experience in automation, mostly with Python. I am also interested in cloud administration, especially Azure. But what i love the most is web development. I have decided to start University focused on software devolpment and i would like to find a job in this field. ",
-  cz: "Jmenuji se Tomáš a pracoval jsem 4 roky jako Linux specialista. Dále mám zkušenost s automatizací, kde používám především Python. Ve své současné práci spravuji Azure prostředí. Co mě ale baví nejvíce je vývoj webu. Rozhodl jsem se začít studovat vysokou školu zaměřenou právě na vývoj softwaru a chtěl bych se tomuto oboru začít věnovat i po kariérní stránce.",
+  en: "My name is Tomáš and i have been working as a Linux adminstrator for four years. I have also experience in automation, mostly with Python. I am also interested in cloud administration, especially Azure. I have also started to study at Unicorn University where i am learning about web development which is something i like to do at my free time. ",
+  cz: "Jmenuji se Tomáš a pracoval jsem 4 roky jako Linux specialista. Dále mám zkušenost s automatizací, kde používám především Python. Ve své současné práci spravuji Azure prostředí. Minulý rok jsem také začal studovat na Unicorn University obor softwarový vývoj, kterému se rád věnuji ve svém volném čase. ",
 };
